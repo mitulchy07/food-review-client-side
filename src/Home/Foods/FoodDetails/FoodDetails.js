9a52626b8f8f413cb1ade9df00690728917a338a
mitulchy07 <mitul9616@gmail.com>
2022-11-08 00:51:20 +0100

    data loaded dynamically

--- src/Home/Foods/FoodDetails/FoodDetails.js
+++ src/Home/Foods/FoodDetails/FoodDetails.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const FoodDetails = () => {
+  const { name, img, description } = useLoaderData();
   return (
     <div>
-      <h1>This is food details</h1>
+      <h1> {name} </h1>
+      <img src={img} alt='' />
+      <p> {description} </p>
     </div>
   );
 };
